6e785563174d7eacab0e1cd10756e540a6882116
CCS-CN3 <120451145+CCS-CN3@users.noreply.github.com>
2024-01-17 17:03:24 +0200

    gg

---
+++ Library.Infrastructure/AppException.cs
@@ -0,0 +1,24 @@
+﻿namespace Library.Infrastructure
+{
+    public class AppException : Exception
+    {
+        public bool IsRecoverable { get; }
+        public bool IsShownToUser { get; }
+
+        public AppException(string message, bool isRecoverable = false, bool isShownToUser = false)
+            : base(message)
+        {
+            IsRecoverable = isRecoverable;
+            IsShownToUser = isShownToUser;
+        }
+
+        public AppException RecoverableException(string message)
+        {
+            return new AppException(message, true, false);
+        }
+        public override string ToString()
+        {
+            return Message;
+        }
+    }
+}
